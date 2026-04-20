06eaa5265a9d74a011c44af14ac9f0dd4a7ca3e0
ismail simsek <ismailsimsek09@gmail.com>
2026-04-20 22:45:35 +0200

    don't touch .config folder

--- .config/jest/mocks/react-inlinesvg.tsx
+++ .config/jest/mocks/react-inlinesvg.tsx
@@ -2,6 +2,8 @@
 // `/public/img/icon/<icon_name>.svg` we need to mock react-inlinesvg to prevent
 // the failed fetch requests from displaying errors in console.
 
+import React from 'react';
+
 type Callback = (...args: any[]) => void;
 
 export interface StorageItem {
