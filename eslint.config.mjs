68435c333553003acdb59b60bfd6e18c1482af60
ismail simsek <ismailsimsek09@gmail.com>
2026-04-20 17:41:57 +0200

    Update configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -36,4 +36,9 @@ export default defineConfig([
     ],
   },
   ...baseConfig,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ]);
--- packages/grafana-prometheus/jest.config.js
+++ packages/grafana-prometheus/jest.config.js
@@ -2,4 +2,9 @@ const sharedConfig = require('../../jest.config.js');
 module.exports = {
   ...sharedConfig,
   rootDir: '../../',
+  modulePaths: ['<rootDir>/packages/grafana-prometheus/src'],
+  testMatch: [
+    '<rootDir>/packages/grafana-prometheus/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/packages/grafana-prometheus/src/**/*.{spec,test,jest}.{js,jsx,ts,tsx}',
+  ],
 };
