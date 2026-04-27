76bca5ead2384e10616cd8bb9cb13bbb4ba4cf5c
ismail simsek <ismailsimsek09@gmail.com>
2026-04-27 17:37:51 +0200

    name updates

--- pkg/main.go
+++ pkg/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	if err := datasource.Manage("grafana-prometheus-datasource", NewDatasource, datasource.ManageOpts{}); err != nil {
+	if err := datasource.Manage("prometheus", NewDatasource, datasource.ManageOpts{}); err != nil {
 		log.DefaultLogger.Error(err.Error())
 		os.Exit(1)
 	}
