75182dea84bc08507aa76dd581ade7cce833b0f4
ismail simsek <ismailsimsek09@gmail.com>
2026-09-26 17:38:16 +0200

    metric combobox: load names alphabetically without metadata

--- packages/grafana-prometheus/src/search_api_client.ts
+++ packages/grafana-prometheus/src/search_api_client.ts
@@ -50,8 +50,11 @@ export interface SearchOptions<T> {
   caseSensitive?: boolean;
 }
 
+export type SearchMetricSort = 'score' | 'alpha';
+
 export interface SearchMetricOptions extends SearchOptions<SearchMetricResult> {
   includeMetadata?: boolean;
+  sortBy?: SearchMetricSort;
 }
 
 type SearchEndpoint = 'metric_names' | 'label_names' | 'label_values';
@@ -183,7 +186,8 @@ export class SearchApiClient extends BaseResourceClient implements ResourceApiCl
   ): Promise<SearchStreamResult<SearchMetricResult>> => {
     return this.trackAvailability(
       this.search('metric_names', timeRange, term, options, {
-        include_metadata: options.includeMetadata ? 'true' : undefined,
+        include_metadata: options.includeMetadata === undefined ? undefined : String(options.includeMetadata),
+        sort_by: options.sortBy,
       })
     );
   };
